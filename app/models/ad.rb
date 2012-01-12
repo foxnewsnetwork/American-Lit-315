f62c731aeef89a5e51bd3e85ea6a9577bf3855ae
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-01-12 14:43:03 -0800

    light fixes

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: ads
+#
+#  id         :integer(4)      not null, primary key
+#  company_id :integer(4)
+#  limit      :integer(4)
+#  type       :integer(4)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Ad < ActiveRecord::Base
+	belongs_to :company
 end
