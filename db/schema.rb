4d21bb3a93f41eb527e5bbc7f3f0804cbe284958
Han Jing <hanqijing@gmail.com>
2012-01-12 17:05:57 +0000

    added Type and Limit profile for each ad

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120111184721) do
+ActiveRecord::Schema.define(:version => 20120112165443) do
 
   create_table "ad_stats", :force => true do |t|
     t.integer  "ad_id"
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(:version => 20120111184721) do
 
   create_table "ads", :force => true do |t|
     t.integer  "company_id"
+    t.integer  "limit"
+    t.integer  "type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  add_index "ads", ["company_id"], :name => "index_ads_on_company_id"
 
   create_table "companies", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
