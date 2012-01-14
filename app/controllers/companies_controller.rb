03f7742048f3d2c597326c2da215607a9cad398f
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-01-13 21:24:12 -0800

    put in dashboard for companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,20 @@
 class CompaniesController < ApplicationController
   def show
+  	@company = Company.find( params[:id] )
+  	@correct_company = false
+  	if company_signed_in?
+  		@correct_company = @company.id == current_company.id
+  	end
+  	@coupons = @company.coupons.paginate( :page => params[:page], :per_page => 10 )
+  	@ads = @company.ads.paginate( :page => params[:page], :per_page => 10 )
+  	@products = @company.products.paginate( :page => params[:page], :per_page => 10 )
   end
 
   def new
   end
 
   def index
+  	@companies = Company.order("name DESC")
   end
 
 end
