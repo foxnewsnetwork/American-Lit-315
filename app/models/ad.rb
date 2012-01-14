6f6989c622cd71cb1439196088487b2eb7e2f1af
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-01-13 22:00:30 -0800

    put in more xml stuff for ads

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,12 +2,23 @@
 #
 # Table name: ads
 #
-#  id         :integer(4)      not null, primary key
-#  company_id :integer(4)
-#  limit      :integer(4)
-#  type       :integer(4)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                  :integer(4)      not null, primary key
+#  company_id          :integer(4)
+#  limit               :integer(4)
+#  type                :integer(4)
+#  created_at          :datetime
+#  updated_at          :datetime
+#  name                :string(255)
+#  description         :text
+#  cost_per_impression :integer(10)
+#  cost_per_click      :integer(10)
+#  cost_per_purchase   :integer(10)
+#  love_hate           :integer(4)
+#  relief_fear         :integer(4)
+#  excite_bore         :integer(4)
+#  happy_sad           :integer(4)
+#  funny_serious       :integer(4)
+#  sexy_disgust        :integer(4)
 #
 
 class Ad < ActiveRecord::Base
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -2,10 +2,16 @@
 #
 # Table name: coupons
 #
-#  id         :integer(4)      not null, primary key
-#  company_id :integer(4)
-#  created_at :datetime
-#  updated_at :datetime
+#  id              :integer(4)      not null, primary key
+#  company_id      :integer(4)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  name            :string(255)
+#  description     :text
+#  cost_per_redeem :integer(10)
+#  limit           :integer(4)
+#  redeemed        :integer(4)
+#  ext_coupon_id   :integer(4)
 #
 
 require 'spec_helper'
