933e6f995ee69ca44531bda573d2b8597501962c
Yuki Nagato <foxnewsnetwork@gmail.com>
2012-01-16 21:29:03 -0800

    made it so that users can now sign up and stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
   def show
+  	@user = User.find( params[:id] )
   end
 
   def new
+  	@user = User.new
   end
 
   def index
+  	@users = User.limit(150).paginate( :page => params[:page], :per_page => 10 )
   end
 
 end
