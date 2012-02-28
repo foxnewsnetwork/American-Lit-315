45f896fa129868cb036d2e5ffc63e802154cdaf9
Han Jing <hanqijing@gmail.com>
2012-02-28 14:40:53 -0800

    added a New York Times test page. Change some stuff up on user modal on the test game. Added shop window to win page for games

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,27 +21,17 @@ class ApplicationController < ActionController::Base
 
 	# TODO:This breaks redirects for publisher and companies also
 	# delete or find new solution
-  def after_sign_out_path_for(users)
-		api_v1_product_inventory_display_path
-  end
+  #def after_sign_out_path_for(users)
+	#	api_v1_product_inventory_display_path
+  #end
 
-  def after_sign_up_path_for(users)
-		api_v1_product_user_shipping_address
-  end
-<<<<<<< HEAD
-	
-  #def after_sign_out_path_for(publisher)
-	#	root_path
+  #def after_sign_up_path_for(users)
+	#	api_v1_product_user_shipping_address
   #end
-  #def after_sign_out_path_for(company)
-	#	root_path
-  #end
-=======
   private
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
     root_path
   end
->>>>>>> 45ff6ee5595767971b9dad6bee321b99eebc53d8
 end
